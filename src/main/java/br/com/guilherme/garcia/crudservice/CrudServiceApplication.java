522d206535aef0ec505b10f003d0581f065d7139
Guilherme Garcia <guilherme.garcia86@gmail.com>
2018-09-26 22:18:50 -0300

    Change annotation

--- src/main/java/br/com/guilherme/garcia/crudservice/CrudServiceApplication.java
+++ src/main/java/br/com/guilherme/garcia/crudservice/CrudServiceApplication.java
@@ -1,13 +1,11 @@
 package br.com.guilherme.garcia.crudservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableAutoConfiguration
+@EnableDiscoveryClient
 public class CrudServiceApplication {
 
 	public static void main(String[] args) {
