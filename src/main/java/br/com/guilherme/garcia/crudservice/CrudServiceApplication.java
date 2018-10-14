3df4e6d4fc8d1e49d997def22410f7d6b28acd74
Guilherme Garcia <guilherme.garcia86@gmail.com>
2018-10-14 20:30:01 -0300

    Add cloud foundry config

--- src/main/java/br/com/guilherme/garcia/crudservice/CrudServiceApplication.java
+++ src/main/java/br/com/guilherme/garcia/crudservice/CrudServiceApplication.java
@@ -3,6 +3,9 @@ package br.com.guilherme.garcia.crudservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,5 +13,11 @@ public class CrudServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudServiceApplication.class, args);
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
